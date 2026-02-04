6879caf24db2c337e03c6f404cbd2eba866e02fa
Irving <tmjh1224@gmail.com>
2026-02-04 16:58:21 +0800

    更新API的bug修正

--- HW/DAY2/DAY2.py
+++ HW/DAY2/DAY2.py
@@ -6,8 +6,8 @@ from langchain_core.runnables import RunnableParallel
 from langchain_core.output_parsers import StrOutputParser
 
 llm = ChatOpenAI(
-    base_url="https://ws-03.wade0426.me/v1", 
-    model="Llama-3.3-70B-Instruct-NVFP4", 
+    base_url="https://ws-03.wade0426.me/v1",  # 注意：LangChain 會自動補上 /chat/completions
+    model="/models/gpt-oss-120b",           # 修改為正確的模型路徑名稱
     temperature=0, 
     api_key="none"
 )
